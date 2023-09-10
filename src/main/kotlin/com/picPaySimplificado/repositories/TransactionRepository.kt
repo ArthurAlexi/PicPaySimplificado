fa106d7cbdc0b4c15530d3bd87f97777d32914be
Arthur Alexi <arthuralexi@hotmail.com>
2023-09-10 15:52:20 -0300

    feat: add TransactionRepository

---
+++ src/main/kotlin/com/picPaySimplificado/repositories/TransactionRepository.kt
@@ -0,0 +1,8 @@
+package com.picPaySimplificado.repositories
+
+import com.picPaySimplificado.domain.transaction.Transaction
+import org.springframework.data.jpa.repository.JpaRepository
+
+class TransactionRepository: JpaRepository<Transaction, Long> {
+
+}
