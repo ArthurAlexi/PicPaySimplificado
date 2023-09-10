7528360a55633ce595fbe7dd2455bd04b291ac56
Arthur Alexi <arthuralexi@hotmail.com>
2023-09-10 15:51:59 -0300

    feat: add UserRepository

---
+++ src/main/kotlin/com/picPaySimplificado/repositories/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.picPaySimplificado.repositories
+
+import com.picPaySimplificado.domain.user.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.Optional
+
+class UserRepository: JpaRepository<User,String>  {
+    fun findUserByDocument(document: String) : Optional<User>
+
+    fun findUserById(id: String) : Optional<User>
+}
